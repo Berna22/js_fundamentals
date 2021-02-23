1055ac612019452d5c847b1ab5cd536e717b09b0
Elena <60672676+Berna22@users.noreply.github.com>
2021-02-23 19:56:19 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -15,7 +15,7 @@ else if (operation == "*"){
     var res = first_number * second_number
     alert("The result is: " + res)
 }
-else {
+else if (operation == "/"){
     var res = first_number / second_number
     if (second_number == 0) {
         alert("Cannot divide by zero!")
