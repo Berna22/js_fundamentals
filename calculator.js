1ce2a2e20a9e1db6363b9b29ce4675bf80cc9c2d
berna <clockworkplum@gmail.com>
2021-02-23 19:46:51 +0100

    Calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+
+var first_number = parseFloat(prompt("Please enter the first number: "))
+var operation = prompt("Please enter operator [+, -, *, /]")
+var second_number = parseFloat(prompt("Please enter the second number: "))
+
+if (operation == "+"){
+    var res = first_number + second_number
+    alert("The result is: " + res)
+}
+else if (operation == "-"){
+    var res = first_number - second_number
+    alert("The result is: " + res)
+}
+else if (operation == "*"){
+    var res = first_number * second_number
+    alert("The result is: " + res)
+}
+else if (operation == "/"){
+    var res = first_number / second_number
+    if (second_number == 0) {
+        alert("Cannot divide by zero!")
+        throw Error("Cannot divide by zero!")
+    }
+    alert("The result is: " + res)
+}
